fd0e1e34857d51165794a0cad845d4c58c966f8e
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-02-06 14:30:45 -0500

    fix(settings): remove accentColor and background from settings payload call

--- src/components/layout/SettingsModal.jsx
+++ src/components/layout/SettingsModal.jsx
@@ -33,9 +33,7 @@ const SettingsModal = ({
             timerSettings,
             autoStart,
             longBreakInterval,
-            accentColor,
-            volume,
-            currentBg
+            volume
         );
 
         try {
