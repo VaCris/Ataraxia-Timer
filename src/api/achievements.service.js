c7df2da482999342a6c6999ab92d07327e05f9de
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-02-28 15:58:32 -0500

    feat(api): add gamification service methods

--- src/api/achievements.service.js
+++ src/api/achievements.service.js
@@ -1,13 +1,24 @@
 import { apiClient } from './client';
 
 export const achievementsService = {
-    getAchievements: async () => {
-        const response = await apiClient.get('/gamification/achievements');
-        return response.data;
+    getLeaderboard: async () => {
+        const res = await apiClient.get('/gamification/leaderboard');
+        return res.data;
     },
-
     getStats: async () => {
-        const response = await apiClient.get('/gamification/stats');
-        return response.data;
+        const res = await apiClient.get('/gamification/stats');
+        return res.data;
+    },
+    getUnlockedAchievements: async () => {
+        const res = await apiClient.get('/gamification/achievements');
+        return res.data;
+    },
+    registerActivity: async (data) => {
+        const res = await apiClient.post('/gamification/test-activity', data);
+        return res.data;
+    },
+    checkAchievements: async () => {
+        const res = await apiClient.post('/gamification/check-achievements');
+        return res.data;
     }
 };
