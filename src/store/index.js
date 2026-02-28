69e83b304b86e6698a16cdad5bb2c14a864440c4
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-02-28 16:37:23 -0500

    feat(store): add achievements reducer and reset state on logout

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import authReducer from './slices/authSlice';
 import timerReducer from './slices/timerSlice';
 import tasksReducer from './slices/tasksSlice';
 import settingsReducer from './slices/settingsSlice';
-
+import achievementsReducer from './slices/achievementsSlice';
 import { rootSaga } from './rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
@@ -13,14 +13,16 @@ const appReducer = combineReducers({
     auth: authReducer,
     timer: timerReducer,
     tasks: tasksReducer,
-    settings: settingsReducer
+    settings: settingsReducer,
+    achievements: achievementsReducer
 });
 
 const rootReducer = (state, action) => {
     if (action.type === 'auth/logout') {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('dw-user');
         state = undefined;
     }
-
     return appReducer(state, action);
 };
 
