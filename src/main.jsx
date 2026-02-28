96b1ed44835867ac1a6ca95c464f863f9bfaeead
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-02-28 16:36:17 -0500

    refactor(main): simplify PWA registration and remove AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { registerSW } from 'virtual:pwa-register'
-import { Provider } from 'react-redux';
-import { store } from './store';
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 registerSW({
   immediate: true,
-  onRegistered(r) {
-    console.log('PWA: Service Worker successfully registered');
-  },
-  onRegisterError(error) {
-    console.error('PWA: Error registering the Service Worker', error);
-  }
-});
+  onRegistered() { },
+  onRegisterError() { }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {/* ELIMINAR <AuthProvider> Y SU CIERRE </AuthProvider> */}
-          <App />
-        {/* ELIMINAR </AuthProvider> */}
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
