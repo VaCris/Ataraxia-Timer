f9314bb29d78ef9238b2ff9c95bc4d709c16d036
Vidal <144495404+VaCris@users.noreply.github.com>
2026-06-16 20:30:55 -0500

    refactor(index.html, main.jsx): format meta tags for consistency and remove unused service worker registration

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { registerSW } from 'virtual:pwa-register'
 import { store } from '@store/index'
 import App from '@/App'
 import './index.css'
-import { startVersionGuard } from '@/shared/version/startVersionGuard'
-
-const updateSW = registerSW({
-  immediate: true,
-  onRegisteredSW(_, registration) {
-    startVersionGuard(registration)
-
-    if (registration) {
-      setInterval(() => {
-        registration.update()
-      }, 30 * 60 * 1000)
-    }
-  },
-  onRegisterError(error) {
-    console.error(error)
-  },
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
