9110e1d1609d0a922040249184159ae4f1f70790
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-09-11 06:33:53 -0500

    test(lint): remove unused act import (#17)

--- src/__tests__/features/auth/components/AuthForm.test.jsx
+++ src/__tests__/features/auth/components/AuthForm.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, act, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { vi } from 'vitest';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
