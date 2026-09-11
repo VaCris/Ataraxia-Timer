5acb0848b8145f73838ad3ac7a5cdf63cf9ca23b
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-09-11 06:54:14 -0500

    test(types): type Framer Motion test mock (#17)

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,21 +3,23 @@ import { vi } from 'vitest';
 import React, { forwardRef } from 'react';
 
 vi.mock('framer-motion', async () => {
-    const actual = await vi.importActual('framer-motion');
+    const actual = await vi.importActual<any>('framer-motion');
+
     return {
         ...actual,
-        AnimatePresence: ({ children }) => React.createElement(React.Fragment, null, children),
+        AnimatePresence: ({ children }: { children: React.ReactNode }) =>
+            React.createElement(React.Fragment, null, children),
         motion: {
             ...actual.motion,
-            div: forwardRef((props, ref) => {
+            div: forwardRef<any, any>((props, ref) => {
                 const { initial, animate, exit, transition, whileHover, whileTap, ...rest } = props;
                 return React.createElement('div', { ref, ...rest });
             }),
-            button: forwardRef((props, ref) => {
+            button: forwardRef<any, any>((props, ref) => {
                 const { initial, animate, exit, transition, whileHover, whileTap, ...rest } = props;
                 return React.createElement('button', { ref, ...rest });
             }),
-            circle: forwardRef((props, ref) => {
+            circle: forwardRef<any, any>((props, ref) => {
                 const { initial, animate, exit, transition, ...rest } = props;
                 return React.createElement('circle', { ref, ...rest });
             }),
