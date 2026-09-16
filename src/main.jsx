05ee585b7bbf31e9c5bc1e12892910bf00be58d8
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-09-16 12:53:56 -0500

    refactor(pwa): mount service worker only in production (#14)

--- src/main.jsx
+++ src/main.jsx
@@ -2,37 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { registerSW } from 'virtual:pwa-register'
 import '@/shared/utils/console-suppress'
 import '@/infrastructure/api/openapi-init'
 import { store } from '@store/index'
 import App from '@/App'
-import { startVersionGuard } from '@/shared/version/startVersionGuard'
+import UpdatePrompt from '@/app/components/UpdatePrompt'
 import './index.css'
 import './styles/ux-hardening.css'
 import './styles/settings-light.css'
-
-registerSW({
-  immediate: true,
-  onRegisteredSW(_, registration) {
-    startVersionGuard(registration)
-
-    if (registration) {
-      setInterval(() => {
-        registration.update()
-      }, 30 * 60 * 1000)
-    }
-  },
-  onRegisterError(error) {
-    console.error(error)
-  },
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        {import.meta.env.PROD && <UpdatePrompt />}
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
